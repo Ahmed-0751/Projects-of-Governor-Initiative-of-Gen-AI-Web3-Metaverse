5999cba22f2cd0bc494fc66d3eebb3f8b4296e2c
Muhammad Ahmed Ali <muhammadahmedali73@gmail.com>
2024-04-03 03:27:22 -0700

    npm package published

--- project01_number guessing game/index.js
+++ project01_number guessing game/index.js
@@ -1,7 +1,10 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // Generate a random number using Random function
 let generatedNum = Math.floor(Math.random() * 10);
+// using loop to contiue the game if guess is wrong
 while (true) {
+    // taking input from user
     const answer = await inquirer.prompt([
         {
             type: "number",
@@ -10,9 +13,10 @@ while (true) {
         }
     ]);
     let ans = answer.guess;
+    // condition to check whether user guessed correct number or not
     if (generatedNum == ans) {
         console.log("Congratulations! You guessed correct number.");
-        break;
+        break; // exit the loop 
     }
     else {
         if (generatedNum > ans) {
